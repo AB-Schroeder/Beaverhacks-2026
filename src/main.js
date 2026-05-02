0f5613484a809806c1974d264f5c8d0ac86f3bb6
Siya Sonpatki <siyasonpatki2@gmail.com>
2026-05-02 15:08:18 -0700

    beaver fixed

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ const k = kaplay();
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 k.loadSprite("bean", "sprites/bean.png");
+k.loadSprite("beaver", "sprites/beaver.png");
+
 
 k.add([k.pos(120, 80), k.sprite("bean")]);
+k.add([k.pos(80, 120), k.sprite("beaver")]);
 
 k.onClick(() => k.addKaboom(k.mousePos()));

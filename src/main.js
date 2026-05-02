2da2f71f42ac7acafabec6bd11bf6cd914826225
Siya Sonpatki <siyasonpatki2@gmail.com>
2026-05-02 15:18:18 -0700

    BRO I GOT IT

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ const k = kaplay();
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 k.loadSprite("bean", "sprites/bean.png");
-k.loadSprite("beaver", "sprites/beaver.png");
-
+//k.loadSprite("beaver", "sprites/beaver.png");
+k.loadSprite("beaver", "sprites/pixilart-sprite.png");
 
 k.add([k.pos(120, 80), k.sprite("bean")]);
 k.add([k.pos(80, 120), k.sprite("beaver")]);
